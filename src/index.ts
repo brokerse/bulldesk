26d259d54fbcd77485018564a7e68cc8e20222a1
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2018-12-19 03:44:14 -0200

    run prettier in src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,27 @@ import axios from 'axios'
 import { ILead } from './interfaces'
 
 const bulldesk = (token: string) =>
-    axios.create({
-        headers: {
-            Accept: 'application/json',
-            Authorization: `Bearer ${token}`,
-        },
-        url: 'https://api.bulldesk.com.br',
-    })
+  axios.create({
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    url: 'https://api.bulldesk.com.br',
+  })
 
 const conversion = (lead: ILead) =>
-    bulldesk()
-        .post('/conversion', lead)
-        .then(result => result)
-        .catch(err => err)
+  bulldesk()
+    .post('/conversion', lead)
+    .then(result => result)
+    .catch(err => err)
 
 const getLeads = (page?: number) =>
-    bulldesk()
-        .get('/leads')
-        .then(result => result)
-        .catch(err => err)
+  bulldesk()
+    .get('/leads')
+    .then(result => result)
+    .catch(err => err)
 
 export default {
-    conversion,
-    getLeads,
+  conversion,
+  getLeads,
 }
